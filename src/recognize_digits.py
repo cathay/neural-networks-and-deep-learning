118e9c031047b0699ee840aac8431512786b66ed
Thay Huynh <thayvn2007@gmail.com>
2017-11-02 18:03:42 +0700

    Add matrix network

--- src/recognize_digits.py
+++ src/recognize_digits.py
@@ -1,8 +1,9 @@
 import mnist_loader
 import network
 
-training_data, validation_data, test_data =  mnist_loader.load_data_wrapper()
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
 
-net = network.Network([784, 100, 10])
+net = network.Network([784, 30, 10])
+#net = network.Network([784, 10])
 net.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
